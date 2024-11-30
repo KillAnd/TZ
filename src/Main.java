cb0d9f36d12ec0cc88ea6795ae99093efe989abd
kila1 <kila14081996@gmail.com>
2024-11-30 16:07:45 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] newNumbers = new int[numbers.length];
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            newNumbers[i] = numbers[numbers.length - 1 - i];
+        }
+
+        System.out.println(Arrays.toString(newNumbers));
+
     }
 }
